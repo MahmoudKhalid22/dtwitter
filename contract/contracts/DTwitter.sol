6e428d46f1524217d12d5723e395e8733bf414eb
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-07-17 01:49:50 +0300

    chore: add smart contract two functions with test

---
+++ contract/contracts/DTwitter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+
+
+
+contract DTwitter {
+    struct User {
+        address wallet;
+        string name;
+        string username;
+        string bio;
+        string avatar;
+    }
+
+    mapping(address => string) public usernames;
+    mapping(string => User) public users;
+
+    function signup(string memory _name, string memory _username, string memory _bio, string memory _avatar) public
+    {
+        require(bytes(usernames[msg.sender]).length == 0, "user already exists");
+        require(users[_username].wallet == address(0), "username is taken, try another one.");
+    
+        users[_username] = User({
+            wallet: msg.sender,
+            name: _name,
+            username: _username,
+            bio: _bio,
+            avatar: _avatar
+        });
+        usernames[msg.sender] = _username;
+    }
+    function getUser(address _wallet) public view returns(User memory){
+        return users[usernames[_wallet]];
+    }
+
+}
--- contract/test/Lock.ts
+++ contract/test/Lock.ts
@@ -4,124 +4,47 @@ import {
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 
 describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+  it("Should return the name", async function () {
+    const DTwitter = await ethers.getContractFactory("DTwitter");
+    const [user1, user2] = await ethers.getSigners();
+    const dtwitter = await DTwitter.deploy();
+    // await dtwitter.deployed();
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+    await dtwitter.signup(
+      "Mahmoud Khalid",
+      "MKhalid",
+      "A web3 Software Engineer",
+      "some url"
+    );
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const user = await dtwitter.users("MKhalid");
+    expect(user.name).to.equal("Mahmoud Khalid");
+    expect(user.avatar).to.equal("some url");
+    expect(user.bio).to.equal("A web3 Software Engineer");
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    console.log("Signing up with the first account Me (mahmoud)");
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    const userFromAddress = await dtwitter.getUser(user1.address);
+    expect(userFromAddress.name).to.equal("Mahmoud Khalid");
+    expect(userFromAddress.avatar).to.equal("some url");
+    expect(userFromAddress.bio).to.equal("A web3 Software Engineer");
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+    expect(await dtwitter.usernames(user1.address)).to.equal("MKhalid");
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
+    console.log("test user already exists");
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    await expect(dtwitter.signup("", "", "", "")).to.be.revertedWith(
+      "user already exists"
+    );
+    await expect(
+      dtwitter
+        .connect(user2)
+        .signup("Mahmoud Khalid", "MKhalid", "SoftWare Engineer", "another url")
+    ).to.be.revertedWith("username is taken, try another one.");
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await hre.ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await hre.ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+    console.log("username is taken error");
   });
 });
